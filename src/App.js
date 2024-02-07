f15baf1c9cfb72a86aeacfc2a56a7a88aac39d94
OlegMaidaniuk <omaydanuk@gmail.com>
2024-02-07 19:13:17 +0200

    lesson2 hw2 1,2 complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {Posts} from "./components/PostsContainer/Posts";
+import {Launches} from "./components/SpaceXContainer/Launches";
 
 const App = () => {
     return (
         <div>
-            <Posts/>
+            {/*<Posts/>*/}
+            <Launches/>
         </div>
     );
 };
--- src/components/SpaceXContainer/Launches.js
+++ src/components/SpaceXContainer/Launches.js
@@ -1,7 +1,19 @@
+import {useEffect, useState} from "react";
+import {launchService} from "../../services/launchService";
+import {Launch} from "./Launch";
+
 const Launches = () => {
+    const [launches, setLaunches] = useState([])
+    useEffect(()=>{
+        launchService.getAll().then(({data})=> {
+            const filterED = data.filter(item=>item.launch_year!=='2020');
+            setLaunches(filterED)
+        })
+    },[])
     return (
         <div>
-            Launches
+            {launches.map(launch=><Launch key={launch.launch_date_unix}
+                                          launch={launch}/>)}
         </div>
     );
 };
